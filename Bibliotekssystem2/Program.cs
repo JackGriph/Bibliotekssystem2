4a3ec104f2e7144900f595b6e70e18d9f31dd67f
Jack Griph <jackgriph111@hotmail.com>
2025-09-23 16:35:28 +0200

    Add project files.

---
+++ Bibliotekssystem2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bibliotekssystem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
