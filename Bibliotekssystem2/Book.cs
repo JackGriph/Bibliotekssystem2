b03129f6d538a049747117cbf90f6382c13011da
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2025-09-24 09:25:12 +0200

    testar konstig vscode respons

--- Bibliotekssystem2/Book.cs
+++ Bibliotekssystem2/Book.cs
@@ -1,4 +1,11 @@
 namespace Bibliotekssystem2
 {
-    
+    public class Book
+    {
+        public string author { get; set; }
+        public string title { get; set; }
+        public int ISBN { get; set; }
+        public bool isBorrowed { get; set; }
+
+    }
 }
