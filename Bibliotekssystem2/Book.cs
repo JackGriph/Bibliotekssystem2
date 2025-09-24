9b588597546f44bc37d7b7b165160806343a4c8f
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2025-09-24 09:27:36 +0200

    lade till konstruktor och isBorrowed ha false som default

--- Bibliotekssystem2/Book.cs
+++ Bibliotekssystem2/Book.cs
@@ -5,7 +5,14 @@ namespace Bibliotekssystem2
         public string author { get; set; }
         public string title { get; set; }
         public int ISBN { get; set; }
-        public bool isBorrowed { get; set; }
+        public bool isBorrowed { get; set; } = false;
+
+        public Book(string title, string author, int ISBN)
+        {
+            this.title = title;
+            this.author = author;
+            this.ISBN = ISBN;
+        }
 
     }
 }
