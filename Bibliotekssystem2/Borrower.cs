864cd85548b28fb375fb6c67eed7fd0d4f9a59f8
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2025-09-24 10:07:13 +0200

    Implementerar ISearchable interfacet

---
+++ Bibliotekssystem2/Borrower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotekssystem2
+{
+    public class Borrower : User, ISearchable { }// Implementerar ISearchable interfacet
+
+    {
+        public Borrower( string name) : base (name) { } 
+        public override void ShowMenu(List<Book> books)
+        {
+            while (true) {
+            Console.WriteLine($"\nVälkommen, {name}! Välj ett alternativ:");
+                Console.WriteLine("1. Sök Bok:");
+            Console.WriteLine("2. Låna bok");
+            Console.WriteLine("3. Lämna tillbaka bok");
+            Console.WriteLine("4. Logga ut");
+            Console.WriteLine("Val: ");
+                var choice = Console.ReadLine();
+            }
+    }
+}
